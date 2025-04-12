40f5e6a3582697fab29e6681cf03ea990b2b10f6
ForgeDom <maxstill2006@gmail.com>
2025-04-12 13:04:56 +0300

    +

--- SystemProgramming2/Program.cs
+++ SystemProgramming2/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace SystemProgramming2;
+﻿using System.Collections;
+
+namespace SystemProgramming2;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        
+
+        List<object> list = new List<object>{"Hello", 1233, "there", DateTime.Now};
+        Thread thread = new Thread(() =>
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+                Thread.Sleep(1000);
+            }
+        });
+        thread.Start();
+        thread.Join();
     }
 }
+
+///////
+//Створіть потік, який «приймає» колекцію елементів, викликає з кожного елемента колекції метод ToString() і виводить результат роботи методу на екран.
+///////
