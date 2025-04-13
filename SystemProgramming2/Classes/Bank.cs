31ca3fe6849aafb807dc431d3c2ed23189645e07
ForgeDom <maxstill2006@gmail.com>
2025-04-13 21:57:05 +0300

    +

---
+++ SystemProgramming2/Classes/Bank.cs
@@ -0,0 +1,49 @@
+﻿namespace SystemProgramming2.Classes;
+
+public class Bank
+{
+    private int _money;
+    private string _name;
+    private int _percent;
+
+    public int Money
+    {
+        get => _money;
+        set
+        {
+            _money = value;
+            WriteToFile();
+        }
+    }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            WriteToFile();
+        }
+    }
+
+    public int Percent
+    {
+        get => _percent;
+        set
+        {
+            _percent = value;
+            WriteToFile();
+        }
+    }
+
+    private void WriteToFile()
+    {
+        Task.Run(() =>
+        {
+            using (StreamWriter writer = new StreamWriter("bank.txt", true))
+            {
+                writer.WriteLine($"Name: {Name}, Money: {Money}, Percent: {Percent}");
+            }
+        });
+    }
+}
--- SystemProgramming2/Program.cs
+++ SystemProgramming2/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace SystemProgramming2;
+﻿using SystemProgramming2.Classes;
+
+namespace SystemProgramming2;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Bank bank = new Bank();
+        bank.Money = 1000;
+        bank.Name = "My Bank";
+        bank.Percent = 5;
+        Console.WriteLine($"Bank Name: {bank.Name}, Money: {bank.Money}, Percent: {bank.Percent}");
         
+        bank.Money = 2000;
+        bank.Name = "Your Bank";
+        bank.Percent = 10;
+        Console.WriteLine($"Updated Bank Name: {bank.Name}, Money: {bank.Money}, Percent: {bank.Percent}");
+        
+        Thread.Sleep(2000);
+        
+        Console.WriteLine("Bank data written to file.");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
+
+///////
+//Створіть клас Bank, в якому будуть наступні властивості: int money, string name, int percent.
+//Побудуйте клас так, щоб при зміні однієї з властивостей класу, створювався новий потік, який записував дані про властивості класу у текстовий файл на жорсткому диску.
+//Клас має інкапсулювати у собі всю логіку багатопотоковості.
+//////
